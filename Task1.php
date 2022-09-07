a52d8751798572b0669f24eb8b0ffaa71549a70d
Artem Artemio <admin@mail.ru>
2022-09-07 12:19:09 +0300

    first  task ended

---
+++ Task1.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tasks\Task1;
+
+use InvalidArgumentException;
+
+class Task1 
+{
+    function main(int $num): string
+    {
+        if (!is_int($num))
+        {
+            throw new InvalidArgumentException('this function only accepts integers. You tried: ' . $num);
+        }
+        return ($num > 30) ? 'More than 30' : (($num > 20) ? 'More than 20' : ($num > 10 ? 'More than 10' : 'Equal or less than 10'));
+    }
+}
+

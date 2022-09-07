76bf15c1a308176deb4fcf5e0a3ffb228b10d447
Artem Artemio <admin@mail.ru>
2022-09-07 15:06:05 +0300

    second task ended

---
+++ Task2.php
@@ -0,0 +1,32 @@
+<?php
+namespace Tasks\Task2;
+
+use InvalidArgumentException;
+
+class Task2
+{
+    function main(string $date) : string
+    {
+        if(!is_string($date)) 
+        {
+            throw new InvalidArgumentException('this function only accepts strings. Input was: '.$date);
+        } 
+        else 
+        {
+            $birth = date_format(date_create($date), 'd-m-Y');
+            $now = date('d-m-Y');
+            $diff = floor((strtotime($birth) - strtotime($now))/86400);
+            if ($diff == 0) 
+            {
+                return 'Today is your birthday, congratulations!';
+            } 
+            else if ($diff < 0 )
+            {
+                return 'Your birthday has already passed ' .-$diff. ' days ago';
+            }
+            return 'It is ' .$diff. ' days before your birthday';
+        }
+    }
+}
+
+

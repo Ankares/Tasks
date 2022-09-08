03c666524f0fa3d7e70895ec985c4621c56e34dc
Artem Artemio <admin@mail.ru>
2022-09-08 17:40:17 +0300

    10 task ended

--- Task10.php
+++ Task10.php
@@ -1,9 +1,34 @@
 <?php
 namespace Tasks\Task10;
 
+use InvalidArgumentException;
+
 class Task10
 {
-    
+    function main(int $input) : array
+    {
+        if(!is_int($input))
+        {
+            throw new InvalidArgumentException('function only accepts integers. Input was: '.$input);
+        }
+        else 
+        {
+            $array = [$input];
+            do
+            {
+                if($input % 2 == 0)
+                {
+                    $input = $input/2;
+                    $array[] = $input;
+                
+                }
+                else
+                {
+                    $input = $input*3+1;
+                    $array[] = $input;
+                }
+            } while($input>1);
+            return $array;
+        }
+    }
 }
-
- 

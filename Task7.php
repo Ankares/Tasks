ce1e1b8d4150b1e98531a913d0255544a5fd9f78
Artem Artemio <admin@mail.ru>
2022-09-08 16:04:17 +0300

    7 task ended

--- Task7.php
+++ Task7.php
@@ -1,9 +1,24 @@
 <?php
 namespace Tasks\Task7;
 
+use InvalidArgumentException;
+
 class Task7
 {
-    
+    function main(array $arr, int $position) : array {
+        if(!is_int($position) && !is_array($arr)) 
+        {
+            throw new InvalidArgumentException('function only accepts integers and arrays.');
+        }
+        else
+        {
+            $newArr = array();
+            for($i=0;$i<count($arr);$i++) {
+                if($arr[$i] != $arr[$position]) {
+                    $newArr[] = $arr[$i];
+                }
+            }
+            return $newArr;
+        }
+    }
 }
-
- 

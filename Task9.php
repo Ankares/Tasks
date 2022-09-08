9a2753ce33e316a015fa4627f340b2a8066633b4
Artem Artemio <admin@mail.ru>
2022-09-08 16:53:12 +0300

    9 task ended

--- Task9.php
+++ Task9.php
@@ -1,9 +1,27 @@
 <?php
 namespace Tasks\Task9;
 
+use InvalidArgumentException;
+
 class Task9
 {
-    
+    function main(array $arr, int $number) : array
+    {
+        if(!is_array($arr) && !is_int($number))
+        {
+            throw new InvalidArgumentException('function only accepts arrays and integers.');
+        }
+        else
+        {
+            $newArr = array();
+            for($i=0;$i<count($arr);$i++)
+            {
+                if($arr[$i]+$arr[$i+1]+$arr[$i+2] == $number)
+                {
+                    $newArr[] = $arr[$i].' + '.$arr[$i+1].' + '.$arr[$i+2];
+                }
+            }
+            return $newArr;
+        }
+    } 
 }
-
- 

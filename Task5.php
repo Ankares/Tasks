d23d5454e6fd72458d88ba632261bdbb406e6767
Artem Artemio <admin@mail.ru>
2022-09-08 13:00:58 +0300

    5 task ended

--- Task5.php
+++ Task5.php
@@ -1,9 +1,25 @@
 <?php
 namespace Tasks\Task5;
 
+use InvalidArgumentException;
+
 class Task5
 {
-    
+    function main(int $n) : int
+    {
+        if(!is_int($n))
+        {
+            throw new InvalidArgumentException('this function only accepts integers. Input was: '.$n);
+        } else 
+        {
+            $prev = 0;
+            $curr = 1;
+            do
+            {
+                $curr = $curr+$prev;
+                $prev = $curr-$prev;
+            } while(strlen($curr)<$n);
+            return $curr;
+        } 
+    }    
 }
-
- 

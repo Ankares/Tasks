577f2c3724ba2b69a92cb90fc98732a8dfd76d66
Artem Artemio <admin@mail.ru>
2022-09-09 12:22:41 +0300

    8 task ended

--- Task8.php
+++ Task8.php
@@ -1,9 +1,29 @@
 <?php
 namespace Tasks\Task8;
 
+use InvalidArgumentException;
+
 class Task8
 {
-    
+    function main(string $json)
+    {
+        if(!is_string($json))
+        {
+            throw new InvalidArgumentException('function only accepts strings. Input was: '.$json);
+        }
+        else
+        {
+            $res = json_decode($json, true, 512);
+            foreach($res as $key => $value)
+            {
+                if(is_array($value))
+                {
+                    foreach($value as $key => $value){
+                        $key. ': ' .$value;
+                    }
+                }
+                echo "$key : $value" . "\r\n";
+            }
+        } 
+    }
 }
-
- 

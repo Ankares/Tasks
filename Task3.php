b5746a1ef718ca440bf12f7c9cfad2e23a403e70
Artem Artemio <admin@mail.ru>
2022-09-09 14:13:10 +0300

    3 task Fixed

--- Task3.php
+++ Task3.php
@@ -1,28 +1,25 @@
 <?php
+
 namespace Tasks\Task3;
 
 use InvalidArgumentException;
 
 class Task3
 {
-    function main(int $number) : int
+    public function main(int $number): int
     {
-        if(!is_int($number))
-        {
+        if (!is_int($number)) {
             throw new InvalidArgumentException('function only accepts integers. Input was: '.$number);
-        }
-        else 
-        {
+        } else {
             $amount = 0;
             $string = (string) $number;
-            for ($i=0;$i<strlen($string);$i++)
-            {
+            for ($i = 0;$i < strlen($string);$i++) {
                 $amount += +$string[$i];
-            }  
-            if ($amount >= 10) 
-            {
+            }
+            if ($amount >= 10) {
                 return $this->main($amount);
             }
+
             return $amount;
         }
     }

e1f7568cf1a2b7d4b09e34d9d0fe84a00f000cff
Artem Artemio <admin@mail.ru>
2022-09-09 15:00:32 +0300

    12 task Fixed

--- Task12.php
+++ Task12.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tasks\Task12;
 
 class MyCalculator
@@ -7,49 +8,57 @@ class MyCalculator
     public $num2;
     public $result;
 
-    public function __construct(int $num1, int $num2) 
+    public function __construct(int $num1, int $num2)
     {
         $this->num1 = $num1;
         $this->num2 = $num2;
     }
-    public function add():MyCalculator
+    public function add(): MyCalculator
     {
-        $this->result = $this->num1+$this->num2;
+        $this->result = $this->num1 + $this->num2;
+
         return $this;
     }
-    public function multiply():MyCalculator
+    public function multiply(): MyCalculator
     {
-        $this->result = $this->num1*$this->num2;
+        $this->result = $this->num1 * $this->num2;
+
         return $this;
     }
-    public function divide():MyCalculator
+    public function divide(): MyCalculator
     {
-        $this->result = $this->num1/$this->num2;
+        $this->result = $this->num1 / $this->num2;
+
         return $this;
     }
-    public function substract():MyCalculator
+    public function substract(): MyCalculator
     {
-        $this->result = $this->num1-$this->num2;
+        $this->result = $this->num1 - $this->num2;
+
         return $this;
     }
-    public function addBy(int $number):MyCalculator
+    public function addBy(int $number): MyCalculator
     {
         $this->result += $number;
+
         return $this;
     }
-    public function divideBy(int $number):MyCalculator
+    public function divideBy(int $number): MyCalculator
     {
         $this->result /= $number;
+
         return $this;
     }
-    public function multiplyBy(int $number):MyCalculator
+    public function multiplyBy(int $number): MyCalculator
     {
         $this->result *= $number;
+
         return $this;
     }
-    public function substractBy(int $number):MyCalculator
+    public function substractBy(int $number): MyCalculator
     {
         $this->result -= $number;
+
         return $this;
     }
 }

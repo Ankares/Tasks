18f7bc19051a942a501650b2b59e5a07b5c1cf8f
Artem Artemio <admin@mail.ru>
2022-09-09 14:31:40 +0300

    6 task Fixed

--- Task6.php
+++ Task6.php
@@ -1,31 +1,27 @@
 <?php
+
 namespace Tasks\Task6;
 
 use InvalidArgumentException;
 
 class Task6
 {
-    function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday') : int
+    public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
     {
-        if(!is_int($year) && !is_int($lastYear) && !is_int($month) && !is_int($lastMonth) && !is_string($day))
-        {
+        if (!is_int($year) && !is_int($lastYear) && !is_int($month) && !is_int($lastMonth) && !is_string($day)) {
             throw new InvalidArgumentException('Incorrect type. Function accepts only integers and strings');
-        }
-        else 
-        {
-            $now = mktime(0,0,0,$month,1,$year);
-            $past = mktime(0,0,0,$lastMonth,1,$lastYear);
+        } else {
+            $now = mktime(0, 0, 0, $month, 1, $year);
+            $past = mktime(0, 0, 0, $lastMonth, 1, $lastYear);
             $count = 0;
-            do
-            {
-                if(date('l',$past) == $day) 
-                {
+            do {
+                if (date('l', $past) == $day) {
                     $count++;
                 }
                 $past += 86400;
-            } while($past<=$now);
+            } while ($past <= $now);
+
             return $count;
         }
     }
 }
- 

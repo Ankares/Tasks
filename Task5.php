2d49b58454f8c8212bfb49d2022bdf5d3a21bf7e
Artem Artemio <admin@mail.ru>
2022-09-09 14:28:21 +0300

    5 task Fixed

--- Task5.php
+++ Task5.php
@@ -1,25 +1,24 @@
 <?php
+
 namespace Tasks\Task5;
 
 use InvalidArgumentException;
 
 class Task5
 {
-    function main(int $n) : int
+    public function main(int $n): int
     {
-        if(!is_int($n))
-        {
+        if (!is_int($n)) {
             throw new InvalidArgumentException('this function only accepts integers. Input was: '.$n);
-        } else 
-        {
+        } else {
             $prev = 0;
             $curr = 1;
-            do
-            {
-                $curr = $curr+$prev;
-                $prev = $curr-$prev;
-            } while(strlen($curr)<$n);
+            do {
+                $curr = $curr + $prev;
+                $prev = $curr - $prev;
+            } while (strlen($curr) < $n);
+
             return $curr;
-        } 
-    }    
+        }
+    }
 }

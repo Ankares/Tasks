6c2ae46f89b2372f4c79f61a842efa092d50ade3
Artem Artemio <admin@mail.ru>
2022-09-09 14:07:55 +0300

    2 task Fixed

--- Task2.php
+++ Task2.php
@@ -1,32 +1,26 @@
 <?php
+
 namespace Tasks\Task2;
 
 use InvalidArgumentException;
 
 class Task2
 {
-    function main(string $date) : string
+    public function main(string $date): string
     {
-        if(!is_string($date)) 
-        {
+        if (!is_string($date)) {
             throw new InvalidArgumentException('this function only accepts strings. Input was: '.$date);
-        } 
-        else 
-        {
+        } else {
             $birth = date_format(date_create($date), 'd-m-Y');
             $now = date('d-m-Y');
-            $diff = floor((strtotime($birth) - strtotime($now))/86400);
-            if ($diff == 0) 
-            {
+            $diff = floor((strtotime($birth) - strtotime($now)) / 86400);
+            if ($diff == 0) {
                 return 'Today is your birthday, congratulations!';
-            } 
-            else if ($diff < 0 )
-            {
+            } elseif ($diff < 0) {
                 return 'Your birthday has already passed ' .-$diff. ' days ago';
             }
+
             return 'It is ' .$diff. ' days before your birthday';
         }
     }
 }
-
-

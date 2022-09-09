6f218b0773fbc8aa9d8d8a13e6915b9db89e8eaa
Artem Artemio <admin@mail.ru>
2022-09-09 14:40:59 +0300

    8 task Fixed

--- Task8.php
+++ Task8.php
@@ -1,24 +1,22 @@
 <?php
+
 namespace Tasks\Task8;
 
 use InvalidArgumentException;
 
 class Task8
 {
-    function main(string $json): array
+    public function main(string $json): array
     {
-        if(!is_string($json))
-        {
+        if (!is_string($json)) {
             throw new InvalidArgumentException('function only accepts strings. Input was: '.$json);
-        }
-        else
-        {
-            $jsonDec = json_decode($json,true,512);
-            array_walk_recursive($jsonDec, function($k,$v)
-            {
-               echo "$v: $k \r\n";
-            });  
-            return $jsonDec;   
+        } else {
+            $jsonDec = json_decode($json, true, 512);
+            array_walk_recursive($jsonDec, function ($k, $v) {
+                echo "$v: $k \r\n";
+            });
+
+            return $jsonDec;
         }
     }
 }

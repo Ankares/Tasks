23f6ef8b1abef595808a1c52371ec8063b5c2c76
Artem Artemio <admin@mail.ru>
2022-09-12 18:36:10 +0300

    task-2

--- Task2.php
+++ Task2.php
@@ -6,21 +6,26 @@ use InvalidArgumentException;
 
 class Task2
 {
-    public function main(string $date): string
+    public function main(string $date): int
     {
         if (!is_string($date)) {
             throw new InvalidArgumentException('this function only accepts strings. Input was: '.$date);
         } else {
-            $birth = date_format(date_create($date), 'd-m-Y');
-            $now = date('d-m-Y');
-            $diff = floor((strtotime($birth) - strtotime($now)) / 86400);
-            if ($diff == 0) {
-                return 'Today is your birthday, congratulations!';
-            } elseif ($diff < 0) {
-                return 'Your birthday has already passed ' .-$diff. ' days ago';
-            }
+            $birthDate = date_create($date);
+            if (preg_match('/^(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-([0-9]{4})$/', $date) == false) {
+                throw new InvalidArgumentException('this function only accepts strings with correct date. Input was: '.$date);
+            } else {
+                $birth = date_format($birthDate, 'd-m-Y');
+                $now = date('d-m-Y');
+                $diff = floor((strtotime($birth) - strtotime($now)) / 86400);
+                if ($diff == 0) {
+                    return 0;
+                } elseif ($diff < 0) {
+                    return 365 + $diff;
+                }
 
-            return 'It is ' .$diff. ' days before your birthday';
+                return $diff;
+            }
         }
     }
 }

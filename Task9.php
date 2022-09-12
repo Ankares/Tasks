9577381f0e0845168e4aa89c20bf5bbe74dbdc5e
Artem Artemio <admin@mail.ru>
2022-09-12 23:54:09 +0300

    task-9

--- Task9.php
+++ Task9.php
@@ -8,12 +8,12 @@ class Task9
 {
     public function main(array $arr, int $number): array
     {
-        if (!is_array($arr) && !is_int($number)) {
-            throw new InvalidArgumentException('function only accepts arrays and integers.');
+        if (!is_array($arr) || !is_int($number) || $number < 1 || count($arr) < 3 || min($arr) < 0) {
+            throw new InvalidArgumentException('array should have at least 3 value and number should be positive.');
         } else {
             $newArr = [];
             for ($i = 0;$i < count($arr);$i++) {
-                if ($arr[$i] + $arr[$i + 1] + $arr[$i + 2] == $number) {
+                if (!empty($arr[$i + 1]) && !empty($arr[$i + 2]) && $arr[$i] + $arr[$i + 1] + $arr[$i + 2] == $number) {
                     $newArr[] = $arr[$i].' + '.$arr[$i + 1].' + '.$arr[$i + 2].' = '.$number;
                 }
             }

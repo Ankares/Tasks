5ed0d4acff106571299d39de72756e4e62e738dc
Artem Artemio <admin@mail.ru>
2022-09-12 21:24:11 +0300

    task-6

--- Task6.php
+++ Task6.php
@@ -8,14 +8,14 @@ class Task6
 {
     public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
     {
-        if (!is_int($year) && !is_int($lastYear) && !is_int($month) && !is_int($lastMonth) && !is_string($day)) {
-            throw new InvalidArgumentException('Incorrect type. Function accepts only integers and strings');
+        if (!is_int($year) || !is_int($lastYear) || !is_int($month) || !is_int($lastMonth) || !is_string($day) || $year <= 0 || $lastYear <= 0 || $month <= 0 || $lastMonth <= 0 || $month > 12 || $lastMonth > 12) {
+            throw new InvalidArgumentException('Incorrect type. Function accepts only positive integers and strings');
         } else {
-            $now = mktime(0, 0, 0, $month, 1, $year);
-            $past = mktime(0, 0, 0, $lastMonth, 1, $lastYear);
+            $past = mktime(0, 0, 0, $month, 1, $year);
+            $now = mktime(0, 0, 0, $lastMonth, 1, $lastYear);
             $count = 0;
             do {
-                if (date('l', $past) == $day) {
+                if (date('d', $past) == 1 && date('l', $past) == $day) {
                     $count++;
                 }
                 $past += 86400;

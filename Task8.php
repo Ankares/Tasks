6c9a0bee832f86857cccc8d739ebf087894bbafa
Artem Artemio <admin@mail.ru>
2022-09-13 11:25:35 +0300

    task-8

--- Task8.php
+++ Task8.php
@@ -11,11 +11,19 @@ class Task8
         if (!is_string($json) || !is_object(json_decode($json))) {
             throw new InvalidArgumentException('function only accepts json. Input was: '.$json);
         } else {
-            $jsonDec = json_decode($json, true, 512);
+            $res = json_decode($json, true, 512);
+            $string = '';
+            foreach ($res as $key => $value) {
+                if (!is_array($value)) {
+                    $string .= "$key: $value" . "\r\n";
+                } else {
+                    foreach ($value as $k => $v) {
+                        $string .= "$k: $v" . "\r\n";
+                    }
+                }
+            }
 
-            return array_walk_recursive($jsonDec, function ($k, $v) {
-                echo "$v: $k\r\n";
-            });
+            return $string;
         }
     }
 }
